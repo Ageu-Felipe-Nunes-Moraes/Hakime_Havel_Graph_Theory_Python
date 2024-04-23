b69b47320326dca45a370a3a309264ee6798e902
Ageu Moraes <ageumoraes67@gmail.com>
2024-04-23 13:22:23 -0300

    Melhorando a acessibilidade do código

--- AlgoritimoTeoriaDosGrafosHakimeHavel/verificaVetorGrafico.py
+++ AlgoritimoTeoriaDosGrafosHakimeHavel/verificaVetorGrafico.py
@@ -4,55 +4,55 @@
 class HakimeHavel:
     
     def __init__(self):
-        self.contador = 0 # Contador para auxilixar
-        self.maior_valor = 0 # Vai receber o maior valor do vetor
+        self.counter = 0 # Counter to help
+        self.highest_value = 0 # Get the highest value of vector
 
-    def reorganizar_vetor(self, vetor): # Organiza a lista em ordem decrescente
-        vetor.sort(reverse=True)
+    def rearrange_vector(self, vector): # Rearrange the list in decrescente order
+        vector.sort(reverse=True)
 
-    def mostra_vetor(self, vetor): # Imprime na tela
-        print(vetor)
+    def show_vector(self, vector): # Print in the terminal
+        print(vector)
 
-    def subtrai_posicoes(self, vetor): # Vai subtrair 1 de cada posição
-        for j in range(self.maior_valor):
-            vetor[j] = vetor[j] - 1 # Subtrai 1 de cada posição
+    def subtract_positions(self, vector): # Will subtract 1 from each position
+        for j in range(self.highest_value):
+            vector[j] = vector[j] - 1 # Subtract 1 from each position
 
-    def verifica_condicoes(self, vetor): # Verifica se é um vetor gráfico ou não
-        for k in range(len(vetor)):
-            if vetor[k] == 0: # Verfica se todas as outras posições também são iguais a "0"
-                self.contador += 1
-                if self.contador == len(vetor): # Cofere a contagem dos zeros 
-                    print(f"É um vetor gráfico: [{vetor}]")
+    def check_conditions(self, vector): # Checks whether it is a vector graphic or not
+        for k in range(len(vector)):
+            if vector[k] == 0: # Checks if all the other positions are equal to the zero as well
+                self.counter += 1
+                if self.counter == len(vector): # Checks the counting of zeros
+                    print(f"É um vetor gráfico: [{vector}]")
                     break
-            if vetor[k] == -1: # Verifica se há algum valor dentro do vetor igual a "-1"
-                print(f"Não é um vetor gráfico: [{vetor}]")
+            if vector[k] == -1: # Checks if there is any value in the from vector that is equal to "-1"
+                print(f"Não é um vetor gráfico: [{vector}]")
                 break
 
         
-    # Função que verifica se é um vetor gráfico por meio dos princípios de Hakime Havel
-    def vetor_grafico(self, vetor):
-        self.reorganizar_vetor(vetor)
-        # Aplicando todas as iterações necessárias para chegar a uma conclusão satisfatória
-        if len(vetor) > vetor[0]: # Verificando se há algum vértice de grau maior que o tamanho do vetor
-            self.mostra_vetor(vetor)
-            for _ in range(len(vetor)):
-                self.maior_valor = vetor[0]
-                del(vetor[0]) # Deleta o primeiro item do vetor
-                self.subtrai_posicoes(vetor)
-                self.mostra_vetor(vetor)
-                self.reorganizar_vetor(vetor)
-                self.mostra_vetor(vetor)
-                if vetor[0] == 0: # Verifica se a primeira posição é igual a "0"
-                    self.verifica_condicoes(vetor)
+    # The function that checks if is a graphic vector using Hakime Havel's principles
+    def graphic_vector(self, vector):
+        self.rearrange_vector(vector)
+        # Applying all necessary iterations to reach a satisfactory conclusion
+        if len(vector) > vector[0]: # Checks if there is any vertices with a degree greater than the size of the vector
+            self.show_vector(vector)
+            for _ in range(len(vector)):
+                self.highest_value = vector[0]
+                del(vector[0]) # Deletes the first item from the vector
+                self.subtract_positions(vector)
+                self.show_vector(vector)
+                self.rearrange_vector(vector)
+                self.show_vector(vector)
+                if vector[0] == 0: # Checks if the first position is "0"
+                    self.check_conditions(vector)
                     break
         else:
             print("Tipo de lista inválida")
 
 hakime = HakimeHavel()
-hakime.vetor_grafico([3,2,2,2,2]) # Lista de entrada
+hakime.graphic_vector([3,2,2,2,2]) # input list
     
 
-#não gráfico
+#not graphic
 #[6, 5, 4, 4, 4, 4, 4, 2]
 #[4, 3, 3, 3, 3, 3, 2]
 #[2, 2, 2, 2, 3, 2]
@@ -66,7 +66,7 @@ hakime.vetor_grafico([3,2,2,2,2]) # Lista de entrada
 #[-1, 0]
 #[0, -1]
 
-# gráfico
+# graphic
 #[5, 4, 3, 3, 3, 3, 3, 2]
 #[3, 2, 2, 2, 2, 3, 2]
 #[3, 3, 2, 2, 2, 2, 2]
